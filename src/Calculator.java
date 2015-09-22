512297037ad812622cc53939d23be42c9b9667c2
user <user@user160>
2015-09-22 18:32:15 +0900

    덧셈 기능 구현

---
+++ src/Calculator.java
@@ -0,0 +1,17 @@
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Calculator calculator = new Calculator();
+		int result  = calculator.add(1,2);
+		System.out.println(result);
+		
+	}
+
+	private int add(int i, int j) {
+		// TODO Auto-generated method stub
+		return i + j;
+	}
+
+}
